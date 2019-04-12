b8705b6e8f42d9d21ca320f6e4078a1be3bc88b5
Travis Exline <t.exline22@gmail.com>
2019-04-12 18:38:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,16 @@ function nestedTarget() {
   return document.querySelector(`#nested .target`);
 }
 
-function increaseRankBy(n) {
+function increaseRankBy(value){
+  const elements = document.querySelectorAll('.ranked-list')
 
+  for (let i = 0, l = elements.length; i < l; i++) {
+    let children = elements[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + value
+    }
+  }
 }
 
 function deepestChild() {
