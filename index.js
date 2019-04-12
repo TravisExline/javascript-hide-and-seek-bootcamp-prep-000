d80e05d56ad097586018914424c62d6301866d62
Travis Exline <t.exline22@gmail.com>
2019-04-12 18:39:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy(value){
+  const elements = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = elements.length; i < l; i++) {
+    let children = elements[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + value
+    }
+  }
+}
+
+function deepestChild() {
+  return document.querySelector(`div#grand-node div div div div`)
+}
