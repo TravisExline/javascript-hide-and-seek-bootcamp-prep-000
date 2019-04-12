6550d4262bea447df0de5fb4d1c18ab09e55a532
Travis Exline <t.exline22@gmail.com>
2019-04-12 15:19:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
-function nestedTarget() {
+function nestedTarget(div) {
   return document.querySelector(div.target);
 }
